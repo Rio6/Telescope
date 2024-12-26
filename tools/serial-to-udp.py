96795e22502566362ae550283a96dfeaee504b79
Rio <rio@r26.me>
2024-12-25 23:19:46 -0500

    add tool for debugging serial protocol

---
+++ tools/serial-to-udp.py
@@ -0,0 +1,18 @@
+import socket
+import serial
+
+HOST = "0.0.0.0"
+PORT = 11880
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((HOST, PORT))
+
+ser = serial.Serial('/dev/ttyFT0', 115200, timeout=0.5)
+
+while True:
+   rdata, addr = sock.recvfrom(10)
+   print('<', rdata)
+   ser.write(rdata)
+   tdata = ser.read_until(b'\r')
+   print('>', tdata)
+   sock.sendto(tdata, addr)
