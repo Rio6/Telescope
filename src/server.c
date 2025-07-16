ff903855d6d98b71c8854a50124daf74a4495e5e
Rio Liu <rio@r26.me>
2025-07-15 22:19:26 -0400

    use synscan udp port

--- src/server.c
+++ src/server.c
@@ -2,28 +2,17 @@
 #include "synscan.h"
 
 #include <esp_log.h>
-#include <nvs_flash.h>
 #include <lwip/sockets.h>
 
-static nvs_handle_t nvs;
 static ss_parser_S server_parser = {0};
 
-typedef struct {
-   in_port_t port;
-} server_config_S;
-
-static server_config_S server_config = {0};
 static int sock;
 
 void server_init(void) {
-   ESP_ERROR_CHECK(nvs_open("server", NVS_READWRITE, &nvs));
-   // TODO nvs load
-   server_config.port = htons(8888);
-
    struct sockaddr_in addr;
    addr.sin_addr.s_addr = htonl(INADDR_ANY);
    addr.sin_family = AF_INET;
-   addr.sin_port = server_config.port;
+   addr.sin_port = htons(11880);
 
    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
    assert(sock >= 0);
@@ -41,7 +30,9 @@ void server_task(void) {
    socklen_t socklen = sizeof(addr);
    ssize_t len = recvfrom(sock, buff, sizeof(buff), 0, (struct sockaddr*) &addr, &socklen);
 
-   ESP_LOGD("server", "%s", buff);
+   if(len > 0) {
+      ESP_LOGD("server", "%s", buff);
+   }
 
    for(int i = 0; i < len; i++) {
       size_t resp_len = ss_handle_byte(&server_parser, buff[i]);
