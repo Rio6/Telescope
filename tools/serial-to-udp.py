d8009f6116eeb7cafd08fc48c518a2fe307287f8
Rio Liu <rio@r26.me>
2025-12-31 12:48:59 -0500

    increase serial-to-udp.py read size

--- tools/serial-to-udp.py
+++ tools/serial-to-udp.py
@@ -8,13 +8,14 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, PORT))
 
 ser = serial.Serial('/dev/ttyFT0', 115200, timeout=0.5)
+ser.timeout = 0.1
 
 while True:
    while ser.in_waiting > 0:
       print('>', ser.read_until(size=ser.in_waiting))
-   rdata, addr = sock.recvfrom(10)
+   rdata, addr = sock.recvfrom(128)
    print('<', rdata)
    ser.write(rdata)
-   tdata = ser.read_until(b'\r')
+   tdata = ser.read(128)
    print('>', tdata)
    sock.sendto(tdata, addr)
